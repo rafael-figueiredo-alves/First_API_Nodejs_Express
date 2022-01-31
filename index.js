ff009fb464b8a7192c3a3653497065d78e7a2c83
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2022-01-31 16:54:05 -0300

    Adição do MongoDB nos métodos

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const {MongoClient, ObjectId} = require('mongodb');
+const express = require("express");
+const { MongoClient, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const db_user = process.env.DB_USER;
@@ -9,64 +9,97 @@ const db_name = process.env.DB_NAME;
 
 const url = `mongodb+srv://${db_user}:${db_pass}@${db_host}`;
 
+async function main() {
+  
+  const Client = await MongoClient.connect(url);
+  
+  const db = Client.db(db_name);
 
+  const Collection = db.collection("Alunos");
 
-const app = express();
+  //Aplicação Express  
+  const app = express();
 
-app.use(express.json());
+  //função para usar JSON no Express
+  app.use(express.json());
 
-app.get('/', function(req, res){
-    res.send('<html><body><h1>Olá mundo!<br>Esta é a minha primeira API usando NodeJS e ExpressJS!</h1></body></html>');
-});
+  //Endpoint principal - exibe página de boas vindas a API
+  app.get("/", function (req, res) {
+    res.send(
+      "<html><body><h1>Olá mundo!</h1><p>Esta é a minha primeira API usando NodeJS e ExpressJS!</p></body></html>"
+    );
+  });
 
-const teste = ['Rafaela', 'Davi', 'Jailza'];
-const alunos = [
-                {
-                    "nome" : "Rafael Alves",
-                    "turma" : "English 5.1"
-                },
-                {
-                    "nome" : "Rafaela Alves",
-                    "turma" : "English 10.1"
-                },
-                {
-                    "nome" : "Davi Alves",
-                    "turma" : "Kids 8.3"
-                },
-                {
-                    "nome" : "Jailza Alves",
-                    "turma" : "English 11.2"
-                }
-               ];
+  //dois testes de Array e Objeto JSON
+  const teste = ["Rafaela", "Davi", "Jailza"];
+  const alunos = [
+    {
+      nome: "Rafael Alves",
+      turma: "English 5.1",
+    },
+    {
+      nome: "Rafaela Alves",
+      turma: "English 10.1",
+    },
+    {
+      nome: "Davi Alves",
+      turma: "Kids 8.3",
+    },
+    {
+      nome: "Jailza Alves",
+      turma: "English 11.2",
+    },
+  ];
 
-app.get('/teste', function(req, res){
+  //Endpoint /teste - exibe o array
+  app.get("/teste", function (req, res) {
     res.send(teste);
-});
+  });
 
-app.get('/alunos', (req, res) => res.send(alunos));
+  //endpoint /aluno para mostrar objeto JSON pré-criado
+  app.get("/aluno", (req, res) => res.send(alunos));
 
-app.post('/alunos', function(req, res){
+  app.get("/alunos", async function (req, res){
+      const documentos = await Collection.find().toArray();
+
+      res.statusCode = 200;
+      res.send(documentos);
+  });
+
+  app.get("/alunos/:id", async function (req, res){
+      const id = req.params.id;
+
+      const item = await Collection.findOne({ _id: new ObjectId(id)});
+
+      res.send(item);
+
+  });
+
+  app.post("/alunos", function (req, res) {
     res.send(req.body);
-});
+  });
 
-app.put('/alunos', function(req, res){
-    res.send('Entrada PUT da entidade alunos');
-});
+  app.put("/alunos", function (req, res) {
+    res.send("Entrada PUT da entidade alunos");
+  });
 
-app.patch('/alunos', function(req, res){
-    res.send('Entrada Patch da entidade alunos');
-});
+  app.patch("/alunos", function (req, res) {
+    res.send("Entrada Patch da entidade alunos");
+  });
 
-app.delete('/alunos', function(req, res){
-    res.send('Entrada Delete da entidade alunos');
-});
+  app.delete("/alunos", function (req, res) {
+    res.send("Entrada Delete da entidade alunos");
+  });
 
-app.options('/alunos', function(req, res){
-    res.send('Entrada Options da entidade Alunos');
-});
+  app.options("/alunos", function (req, res) {
+    res.send("Entrada Options da entidade Alunos");
+  });
 
-app.head('/alunos', function(req, res){
-    res.send('Entrada Head da Entidade Alunos');
-});
+  app.head("/alunos", function (req, res) {
+    res.send("Entrada Head da Entidade Alunos");
+  });
 
-app.listen(process.env.PORT || 3000);
+  app.listen(process.env.PORT || 3000);
+}
+
+main();
